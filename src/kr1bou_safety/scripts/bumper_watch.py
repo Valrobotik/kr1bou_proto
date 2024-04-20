094734a1e733cf82babe9d41d77a17760b184604
valrob <valrobotik@gmail.com>
2024-04-20 16:04:29 +0200

    changed Bool to Byte for more accurate information

--- src/kr1bou_safety/scripts/bumper_watch.py
+++ src/kr1bou_safety/scripts/bumper_watch.py
@@ -1,43 +1,46 @@
 #!/usr/bin/env python3
 """
 Looks out for bumpers signals on specific GPIO pins. Handles events such as the robot reaching a wall.
-When two front or two back buttons are held, it publishes a True signal on the 'bumper' topic.
+When two front or two back buttons are held, it publishes on the 'bumper' topic.
 """
 
 import rospy
 from gpiozero import Button
-from std_msgs.msg import Bool
+from std_msgs.msg import Byte
+from typing import Tuple
 
 def setup_buttons(pins):
     return [Button(pin) for pin in pins]
 
 
-def check_bumpers(front_buttons, back_buttons):
+def check_bumpers(buttons: Tuple[Button]):
     # Check if both front or both back buttons are pressed
-    front_pressed = all(button.is_pressed for button in front_buttons)
-    back_pressed = all(button.is_pressed for button in back_buttons)
-    return front_pressed or back_pressed
+    state = Byte()
+    state.data = 0
+    for i, button in enumerate(buttons):
+        if button.is_pressed:
+            state.data += 2**i
+    return state
 
 
-def publish_bumper_event(front_pins, back_pins, frequency):
-    front_buttons = setup_buttons(front_pins)
-    back_buttons = setup_buttons(back_pins)
+def publish_bumper_event(bumper_pins, frequency):
+    buttons = setup_buttons(bumper_pins)
     
-    pub = rospy.Publisher('bumper', Bool, queue_size=10)
+    pub = rospy.Publisher('bumper', Byte, queue_size=10)
     rospy.init_node('bumper_watch', anonymous=True)
     rate = rospy.Rate(frequency)
 
     while not rospy.is_shutdown():
-        bumper_pressed = check_bumpers(front_buttons, back_buttons)
-        pub.publish(bumper_pressed)
+        bumpers_pressed = check_bumpers(buttons)
+        rospy.loginfo(bumpers_pressed)
+        pub.publish(bumpers_pressed)
         rate.sleep()
 
 
 if __name__ == '__main__':
     try:
-        front_pins = rospy.get_param('/gpio/front_bumper_pins')
-        back_pins = rospy.get_param('/gpio/back_bumper_pins')
-        frequency = rospy.get_param('/frequency', 10)  # Default to 10 Hz if not set
-        publish_bumper_event(front_pins, back_pins, frequency)
+        bumper_pins = rospy.get_param('/gpio/bumper_pins')
+        frequency = rospy.get_param('/frequency')
+        publish_bumper_event(bumper_pins, frequency)
     except rospy.ROSInterruptException as e:
         rospy.logerr(e)
