0ad2706c40eef4b4704d9de305b4403bf1d598e9
valrob <valrobotik@gmail.com>
2024-04-21 16:23:26 +0200

    updated the state manager

--- src/kr1bou_controller/scripts/state_manager.py
+++ src/kr1bou_controller/scripts/state_manager.py
@@ -14,7 +14,7 @@ def identify_arduino_ports(known_sensors):
     identified_ports = {}
     for port, desc, hwid in sorted(ports):
         ser = serial.Serial(port)  # Open the port
-        ser.write('identify\n'.encode())  # Send command to get sensor ID response
+        ser.write('NR\n'.encode())  # Send command to get sensor ID response
         line = ser.readline()
         sensor_id = line.decode().strip()
         if sensor_id in known_sensors:
@@ -24,7 +24,8 @@ def identify_arduino_ports(known_sensors):
 
 
 def config_callback(msg):
-    if msg.data:  # True when the starter key/button is pressed
+    queue_size = rospy.get_param('/queue_size')
+    if msg.data is False:  # We want the key to be released
         rospy.loginfo("Config phase started")
 
         known_sensors = rospy.get_param('/arduino/known_sensors')
@@ -33,15 +34,40 @@ def config_callback(msg):
 
         # Dump identified ports to ROS parameters
         for sensor_id, port in identified_ports.items():
-            rospy.set_param(f'arduino/arduino_serial_ports/uS_{sensor_id}', port)
+            rospy.set_param(f'/arduino/arduino_serial_ports/{sensor_id}', port)
+            rospy.loginfo(f"Arduino sensor {sensor_id} connected to port {port}")
 
         rospy.loginfo("Configuration phase completed. Identified ports dumped to ROS parameters.")
-        # Transition to the next state if necessary, e.g., publish on a 'state_change' topic
+        # Transition to the next state via publisher
+        pub = rospy.Publisher('configPhase', Bool, queue_size=queue_size)
+        pub.publish(False)
+        pub.unregister()
+        pub = rospy.Publisher('runingPhase', Bool, queue_size=queue_size)
+        pub.publish(True)
+        pub.unregister()
+
+
+def running_callback(msg):
+    # TODO : Implement the running phase logic, if any
+    pass
+
+
+def stop_callback(msg):
+    # TODO : Implement the stop logic. Send stop data to the actuator nodes ?
+    pass
+
+
+def emergency_callback(msg):
+    # TODO : Implement the emergency stop logic. Send emergency stop data to the actuator nodes ? / Stop all nodes ?
+    pass
+
 
 if __name__ == '__main__':
     rospy.init_node('state_manager')
 
     rospy.Subscriber('configPhase', Bool, config_callback)
+    rospy.Subscriber('runningPhase', Bool, running_callback)
+    rospy.Subscriber('stop', Bool, stop_callback)
+    rospy.Subscriber('emergencyStop', Bool, emergency_callback)
 
-    rospy.loginfo("State manager node initialized, awaiting config phase.")
     rospy.spin()
