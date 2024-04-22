81e5183fd9976ffd0335d7349376e9ed0de42a9f
valrob <valrobotik@gmail.com>
2024-04-22 20:11:23 +0200

    upload ultrason

--- src/kr1bou_sensors/scripts/ultrasound.py
+++ src/kr1bou_sensors/scripts/ultrasound.py
@@ -73,30 +73,30 @@ if __name__ == '__main__':
     rospy.init_node('ultrasound_sensor_manager')
     rospy.loginfo("[START] Ultrasound Sensor node has started.")
 
-    # # Load configuration parameters
-    # frequency = rospy.get_param('/frequency')
-    # queue_size = rospy.get_param('/queue_size')
-    # baudrate = rospy.get_param('/arduino/baudrate')
-    # map_boundaries = rospy.get_param('/map_boundaries')  # (x_min, y_min, x_max, y_max)
+    # Load configuration parameters
+    frequency = rospy.get_param('/frequency')
+    queue_size = rospy.get_param('/queue_size')
+    baudrate = rospy.get_param('/arduino/baudrate')
+    map_boundaries = rospy.get_param('/map_boundaries')  # (x_min, y_min, x_max, y_max)
 
-    # rate = rospy.Rate(frequency)
-    # # Wait for the runningPhase True signal
-    # rospy.Subscriber('runningPhase', Bool, run)
-    # while not start:
-    #     rate.sleep()
+    rate = rospy.Rate(frequency)
+    # Wait for the runningPhase True signal
+    rospy.Subscriber('runningPhase', Bool, run)
+    while not start:
+        rate.sleep()
     
-    # # Load sensor parameters
-    # sensor_positions = rospy.get_param('/sensor_positions/uS')  # [(x, y, z, angle), ...]. Angle is in radians
-    # serial_port_param = rospy.get_param(f'/arduino/arduino_serial_ports/uS')
-    # serial_port = serial.Serial(serial_port_param, baudrate, timeout=1)
+    # Load sensor parameters
+    sensor_positions = rospy.get_param('/sensor_positions/uS')  # [(x, y, z, angle), ...]. Angle is in radians
+    serial_port_param = rospy.get_param(f'/arduino/arduino_serial_ports/uS')
+    serial_port = serial.Serial(serial_port_param, baudrate, timeout=1)
 
-    # # Publisher and Subscriber
-    # sensor_data_pub = rospy.Publisher('ultrasound_sensor_data', Float32MultiArray, queue_size=queue_size)
-    # rospy.Subscriber('pose', Pose2D, pose_callback)
+    # Publisher and Subscriber
+    sensor_data_pub = rospy.Publisher('ultrasound_sensor_data', Float32MultiArray, queue_size=queue_size)
+    rospy.Subscriber('pose', Pose2D, pose_callback)
 
-    # try:
-    #     read_and_publish_sensor_data()
-    # except rospy.ROSInterruptException:
-    #     pass
-    # finally:
-    #     serial_port.close()
+    try:
+        read_and_publish_sensor_data()
+    except rospy.ROSInterruptException:
+        pass
+    finally:
+        serial_port.close()
