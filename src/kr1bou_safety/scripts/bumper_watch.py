4ceaf2620c1db069b2610e1e7654b866a27eb077
valrob <valrobotik@gmail.com>
2024-04-22 15:20:33 +0200

    debug

--- src/kr1bou_safety/scripts/bumper_watch.py
+++ src/kr1bou_safety/scripts/bumper_watch.py
@@ -32,7 +32,7 @@ def publish_bumper_event(bumper_pins):
 
     while not rospy.is_shutdown():
         bumpers_pressed = check_bumpers(buttons)
-        rospy.loginfo(bumpers_pressed)
+        #rospy.loginfo(bumpers_pressed)
         pub.publish(bumpers_pressed.data)
         rate.sleep()
 
