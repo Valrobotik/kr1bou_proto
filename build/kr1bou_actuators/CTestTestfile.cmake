562e902d959664f8bb614eae8121e6cbc43523fc
valrob <valrobotik@gmail.com>
2024-04-23 17:42:58 +0200

    supress devel and build

--- build/kr1bou_actuators/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/valrob/code/kr1bou_proto/src/kr1bou_actuators
-# Build directory: /home/valrob/code/kr1bou_proto/build/kr1bou_actuators
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/kr1bou_launch/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/valrob/code/kr1bou_proto/src/kr1bou_launch
-# Build directory: /home/valrob/code/kr1bou_proto/build/kr1bou_launch
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
