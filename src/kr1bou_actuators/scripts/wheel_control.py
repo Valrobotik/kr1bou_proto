acfa98fb094f68626e3bf57bf959e094102b5c72
valrob <valrobotik@gmail.com>
2024-04-24 16:03:59 +0200

    add encode

--- src/kr1bou_actuators/scripts/wheel_control.py
+++ src/kr1bou_actuators/scripts/wheel_control.py
@@ -64,7 +64,7 @@ class WheelController:
                     for i in self.list_comande_to_send:
                         str += i
                         self.list_comande_to_send.pop(self.list_comande_to_send.index(i))
-                    self.serial_port.write(str)
+                    self.serial_port.write(str.encode())
                 except serial.SerialException as s:
                     rospy.logwarn("Error while sending correction to Arduino.")
                     rospy.logwarn(s)    
