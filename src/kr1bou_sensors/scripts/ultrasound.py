b8a5dd9013f82c53c64dd302f2bc44d6151fb97b
valrob <valrobotik@gmail.com>
2024-04-28 12:26:28 +0200

    debug

--- src/kr1bou_sensors/scripts/ultrasound.py
+++ src/kr1bou_sensors/scripts/ultrasound.py
@@ -116,7 +116,7 @@ if __name__ == '__main__':
     rospy.Subscriber('odometry', Pose2D, pose_callback)
 
     try:
-        # read_and_publish_sensor_data()
+        read_and_publish_sensor_data()
         pass
     except rospy.ROSInterruptException:
         pass
