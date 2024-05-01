3c7c215bab72a3339016d953c8b4bd3933bba186
valrob <valrobotik@gmail.com>
2024-05-01 13:32:08 +0200

    debug

--- src/kr1bou_controller/scripts/strategie.py
+++ src/kr1bou_controller/scripts/strategie.py
@@ -111,7 +111,7 @@ if __name__ == "__main__":
     rate = rospy.Rate(rospy.get_param('/frequency'))
     while not start:
         rate.sleep()
-    rospy.sleep(0.5)
+    rospy.sleep(3)
     strat = strategie()
     strat.run()
 
