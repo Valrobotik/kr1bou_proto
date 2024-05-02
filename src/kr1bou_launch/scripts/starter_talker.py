716167cfacc2b91c31b0a8c2c246a7d3cdfa1a20
kribou <elias.boulanger1@gmail.com>
2024-05-03 00:00:43 +0200

    removed state manager and fixed issue with get/set arduino ports

--- src/kr1bou_launch/scripts/starter_talker.py
+++ src/kr1bou_launch/scripts/starter_talker.py
@@ -9,12 +9,14 @@ from gpiozero import Button
 from std_msgs.msg import Bool
 
 
+# Logic for key press and release are reversed because of the hardware architecture.
 def on_key_press():
     rospy.loginfo("Key removed: True")
     pub.publish(True)
 
+
 def on_key_release():
-    rospy.loginfo("Key inssert: False")
+    rospy.loginfo("Key inserted: False")
 
 
 if __name__ == '__main__':
@@ -24,7 +26,7 @@ if __name__ == '__main__':
         start_pin = rospy.get_param('/gpio/start_button_pin')
         queue_size = rospy.get_param('/queue_size')
         
-        pub = rospy.Publisher('configPhase', Bool, queue_size=queue_size)
+        pub = rospy.Publisher('runningPhase', Bool, queue_size=queue_size)
         key = Button(int(start_pin))
         key.when_pressed = on_key_press
         key.when_released = on_key_release
