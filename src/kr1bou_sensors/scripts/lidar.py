22ff2b8ba2b4a7c1220fd5136a8926d8747a99be
kribou <elias.boulanger1@gmail.com>
2024-05-03 19:34:07 +0200

    cleaned karibou sensors

--- src/kr1bou_sensors/scripts/lidar.py
+++ src/kr1bou_sensors/scripts/lidar.py
@@ -3,57 +3,39 @@
 Manages the data of the lidar.
 """
 
-import serial
-
-from hokuyo.driver import hokuyo
-from hokuyo.tools import serial_port
-
 import math
 
-import time
-
-from sklearn.cluster import DBSCAN
 import numpy as np
-
-import serial
-
-from hokuyo.driver import hokuyo
-from hokuyo.tools import serial_port
-
-import math
-
-import time
-
-from sklearn.cluster import DBSCAN
-import numpy as np
-
-
 import rospy
-
+import serial
 from geometry_msgs.msg import PoseArray, Pose, Pose2D
+from sklearn.cluster import DBSCAN
 from std_msgs.msg import Bool
+
+from hokuyo.driver import hokuyo
+from hokuyo.tools import serial_port
 
 uart_port = rospy.get_param("/arduino/arduino_serial_ports/Lidar")
 uart_speed = 19200
+
 
 def get_position(data: Pose2D):
     global x_robot, y_robot, theta_robot
     x_robot = data.x
     y_robot = data.y
     theta_robot = data.theta
-    
+
+
 def run(data: Bool):
     global start
     start = data.data
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     rospy.init_node("Lidar", anonymous=True)
+    rospy.loginfo("[START] Lidar node has started.")
     pub_data = rospy.Publisher("lidar_data", PoseArray, queue_size=1)
     rospy.Subscriber("pose", Pose2D, get_position)
-
-    
-    rospy.loginfo("[START] Lidar node has started.")
 
     laser_serial = serial.Serial(port=uart_port, baudrate=uart_speed, timeout=0.5)
     port = serial_port.SerialPort(laser_serial)
@@ -75,21 +57,24 @@ if __name__ == '__main__':
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
-        dict = laser.get_scan()
+        dictionary = laser.get_scan()
         x = []
         y = []
-        for i in range(0,len(dict[0])):
-            if(dict[1][i]>90):
-                x_temp = dict[1][i]/1000*math.sin(math.radians(dict[0][i])-theta_robot+math.pi/2)+x_robot
-                y_temp = dict[1][i]/1000*math.cos(math.radians(dict[0][i])-theta_robot+math.pi/2)+y_robot
-                if x_temp>0 and x_temp<3 and y_temp>0 and y_temp<2:
+        for i in range(0, len(dictionary[0])):
+            if dictionary[1][i] > 90:
+                x_temp = (dictionary[1][i] / 1000 * math.sin(math.radians(dictionary[0][i]) - theta_robot + math.pi / 2)
+                          + x_robot)
+                y_temp = (dictionary[1][i] / 1000 * math.cos(math.radians(dictionary[0][i]) - theta_robot + math.pi / 2)
+                          + y_robot)
+                if 0 < x_temp < 3 and 0 < y_temp < 2:
                     x.append(x_temp)
                     y.append(y_temp)
-        
+
         points = np.array([x, y]).T
 
-        # Appliquer DBSCAN sur les points. eps est la distance maximale entre deux échantillons pour qu'ils soient considérés comme dans le même voisinage.
-        try :
+        # Appliquer DBSCAN sur les points. eps est la distance maximale entre deux échantillons pour qu'ils soient
+        # considérés comme dans le même voisinage.
+        try:
             db = DBSCAN(eps=0.06, min_samples=3).fit(points)
 
             labels = db.labels_
@@ -101,7 +86,8 @@ if __name__ == '__main__':
                     group_points = points[labels == group_id]
                     group_mean = group_points.mean(axis=0)
                     groups.append(group_mean)
-        except:
+        except Exception as e:
+            print(e)
             groups = []
 
         PoseArray_msg = PoseArray()
@@ -113,23 +99,19 @@ if __name__ == '__main__':
             PoseArray_msg.poses.append(Pose_msg)
         pub_data.publish(PoseArray_msg)
         rate.sleep()
-import rospy
 
-from geometry_msgs.msg import PoseArray, Pose, Pose2D
-from std_msgs.msg import Bool
-
-uart_port = '/dev/ttyACM0'
-uart_speed = 19200
 
 def get_position(data: Pose2D):
     global x_robot, y_robot, theta_robot
     x_robot = data.x
     y_robot = data.y
     theta_robot = data.theta
-    
+
+
 def run(data: Bool):
     global start
     start = data.data
+
 
 if __name__ == '__main__':
 
@@ -137,7 +119,6 @@ if __name__ == '__main__':
     pub_data = rospy.Publisher("lidar_data", PoseArray, queue_size=1)
     rospy.Subscriber("pose", Pose2D, get_position)
 
-    
     rospy.loginfo("[START] Lidar node has started.")
 
     laser_serial = serial.Serial(port=uart_port, baudrate=uart_speed, timeout=0.5)
@@ -160,23 +141,22 @@ if __name__ == '__main__':
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
-        dict = laser.get_scan()
+        dictionary = laser.get_scan()
         x = []
         y = []
-        for i in range(0,len(dict[0])):
-            if(dict[1][i]>90):
-                x_temp = dict[1][i]/1000*math.sin(math.radians(dict[0][i])-theta_robot+math.pi/2)+x_robot
-                y_temp = dict[1][i]/1000*math.cos(math.radians(dict[0][i])-theta_robot+math.pi/2)+y_robot
-                if x_temp>0 and x_temp<3 and y_temp>0 and y_temp<2:
+        for i in range(0, len(dictionary[0])):
+            if dictionary[1][i] > 90:
+                x_temp = dictionary[1][i] / 1000 * math.sin(math.radians(dictionary[0][i]) - theta_robot + math.pi / 2) + x_robot
+                y_temp = dictionary[1][i] / 1000 * math.cos(math.radians(dictionary[0][i]) - theta_robot + math.pi / 2) + y_robot
+                if 0 < x_temp < 3 and 0 < y_temp < 2:
                     x.append(x_temp)
                     y.append(y_temp)
-        
+
         points = np.array([x, y]).T
-
-        # Appliquer DBSCAN sur les points. eps est la distance maximale entre deux échantillons pour qu'ils soient considérés comme dans le même voisinage.
-        try :
+        # Appliquer DBSCAN sur les points. eps est la distance maximale entre deux échantillons pour qu'ils soient
+        # considérés comme dans le même voisinage.
+        try:
             db = DBSCAN(eps=0.06, min_samples=3).fit(points)
-
             labels = db.labels_
 
             # Pour chaque groupe, calculer le point moyen et l'ajouter à la liste des groupes.
@@ -186,7 +166,8 @@ if __name__ == '__main__':
                     group_points = points[labels == group_id]
                     group_mean = group_points.mean(axis=0)
                     groups.append(group_mean)
-        except:
+        except Exception as e:
+            print(e)
             groups = []
 
         PoseArray_msg = PoseArray()
--- src/kr1bou_sensors/scripts/test_lidar.py
+++ src/kr1bou_sensors/scripts/test_lidar.py
@@ -1,20 +1,15 @@
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
 import serial
+from sklearn.cluster import DBSCAN
 
 from hokuyo.driver import hokuyo
 from hokuyo.tools import serial_port
 
-import matplotlib.pyplot as plt
-import math
-
-import time
-
-from sklearn.cluster import DBSCAN
-import numpy as np
-
-
 uart_port = '/dev/ttyACM0'
 uart_speed = 19200
-
 
 if __name__ == '__main__':
 
@@ -39,20 +34,19 @@ if __name__ == '__main__':
         dict = laser.get_scan()
         x = []
         y = []
-        for i in range(0,len(dict[0])):
-            if(dict[1][i]>90):
-                x_temp = dict[1][i]/1000*math.sin(math.radians(dict[0][i])-theta_robot+math.pi/2)+x_robot
-                y_temp = dict[1][i]/1000*math.cos(math.radians(dict[0][i])-theta_robot+math.pi/2)+y_robot
-                if x_temp>0 and x_temp<3 and y_temp>0 and y_temp<2:
+        for i in range(0, len(dict[0])):
+            if (dict[1][i] > 90):
+                x_temp = dict[1][i] / 1000 * math.sin(math.radians(dict[0][i]) - theta_robot + math.pi / 2) + x_robot
+                y_temp = dict[1][i] / 1000 * math.cos(math.radians(dict[0][i]) - theta_robot + math.pi / 2) + y_robot
+                if x_temp > 0 and x_temp < 3 and y_temp > 0 and y_temp < 2:
                     x.append(x_temp)
                     y.append(y_temp)
-        
+
         points = np.array([x, y]).T
-
-        # Appliquer DBSCAN sur les points. eps est la distance maximale entre deux échantillons pour qu'ils soient considérés comme dans le même voisinage.
-        try :
+        # Appliquer DBSCAN sur les points. eps est la distance maximale entre deux échantillons pour qu'ils soient
+        # considérés comme dans le même voisinage.
+        try:
             db = DBSCAN(eps=0.06, min_samples=3).fit(points)
-
             labels = db.labels_
 
             # Pour chaque groupe, calculer le point moyen et l'ajouter à la liste des groupes.
@@ -62,11 +56,11 @@ if __name__ == '__main__':
                     group_points = points[labels == group_id]
                     group_mean = group_points.mean(axis=0)
                     groups.append(group_mean)
-        except:
+        except Exception as e:
+            print(e)
             groups = []
 
         # Afficher les groupes
-
         plt.plot(x, y, '.', color='b')
         for group in groups:
             plt.plot(group[0], group[1], 'o', color='r')
@@ -78,7 +72,6 @@ if __name__ == '__main__':
         plt.draw()  # Dessine la figure sans bloquer l'exécution
         plt.pause(0.01)  # Pause de 2 secondes
         plt.clf()  # Efface la figure pour le prochain tour de boucle
-        
 
     plt.ioff()  # Désactive le mode interactif
     plt.show()
