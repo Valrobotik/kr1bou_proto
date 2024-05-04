740fcc8c970818a8593e3e7dd0a6416111cfe183
valrob <valrobotik@gmail.com>
2024-05-04 12:27:02 +0200

    debug

--- src/kr1bou_controller/scripts/strategy.py
+++ src/kr1bou_controller/scripts/strategy.py
@@ -102,11 +102,11 @@ class Strategy:
         the form {direction: (cost, neighbor_node)}. The cost is very high if the neighbor is an obstacle.
         :return: the path to follow
         """
-        rospy.loginfo(f"Data : \nL: {self.lidar_data}, \nUS: {self.us_data}, \nC: {self.camera_position}")
+        #rospy.loginfo(f"Data : \nL: {self.lidar_data}, \nUS: {self.us_data}, \nC: {self.camera_position}")
         self.obstacles = get_discrete_obstacles(self.lidar_data, self.us_data,
                                                     [(self.enemy_position.x, self.enemy_position.y)],
                                                     self.resolution, self.radius, self.map_boundaries)
-        rospy.loginfo(f"Obstacles : {self.obstacles}")
+        #rospy.loginfo(f"Obstacles : {self.obstacles}")
         self.maze = update_maze(self.maze, self.previous_obstacles, self.obstacles)
         self.previous_obstacles = self.obstacles
 
