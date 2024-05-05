b42dfe10dbb29ebda3734b401a0dcb34185bcf83
valrob <valrobotik@gmail.com>
2024-05-06 01:59:52 +0200

    debug

--- src/kr1bou_safety/scripts/emergency_stop.py
+++ src/kr1bou_safety/scripts/emergency_stop.py
@@ -70,13 +70,10 @@ def list_of_obstacles():
 
     for obstacle in US_obstacles:
         obstacles.append((obstacle[0], obstacle[1]))
-    
-    rospy.loginfo(f"{type(obstacles[0][0])}")
 
     for i in range(len(obstacles)):
         obstacles[i] = ((obstacles[i][0] - robot_position.x)*math.cos(robot_position.theta), (obstacles[i][1] - robot_position.y)*math.sin(robot_position.theta))
 
-    rospy.loginfo(f"{type(obstacles[0][0])}")
     return obstacles
     
 
@@ -89,13 +86,10 @@ def emergency_stop_run():
         emergency_front = False
         emergency_back = False
         for obstacle in obstacles:
-            try :
-                if obstacle[0]>0.0 and obstacle[0]<0.3 and obstacle[1]>-0.2 and obstacle[1]<0.2:
-                    emergency_front = True
-                if obstacle[0]>-0.3 and obstacle[0]<0.0 and obstacle[1]>-0.2 and obstacle[1]<0.2:
-                    emergency_back = True
-            except:
-                rospy.loginfo(f"obstacle : {obstacle}")
+            if obstacle[0]>0.0 and obstacle[0]<0.3 and obstacle[1]>-0.2 and obstacle[1]<0.2:
+                emergency_front = True
+            if obstacle[0]>-0.3 and obstacle[0]<0.0 and obstacle[1]>-0.2 and obstacle[1]<0.2:
+                emergency_back = True
         if bumper_1_front or bumper_2_front:
             emergency_front = True
         if bumper_3_back or bumper_4_back:
