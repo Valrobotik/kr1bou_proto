f51a0fe7e3b99708320f6a88238760d18e8b14f8
valrob <valrobotik@gmail.com>
2024-05-10 00:21:01 +0200

    test

--- src/kr1bou_controller/scripts/strategy.py
+++ src/kr1bou_controller/scripts/strategy.py
@@ -496,9 +496,9 @@ class Strategy:
     # -- DEBUG --
     def debug_phase_goto(self):
         while not rospy.is_shutdown():
-            self.go_to(2.55, 0.95, 3.49, MAX_SPEED, 1)
+            self.go_to(2.55, 1.05, 2.79, MAX_SPEED, 1)
             self.wait_until_ready()
-            self.go_to(.7, 0.37, 3.54, MEDIUM_SPEED, 1)
+            self.go_to(.7, 2.63, 2.74, MEDIUM_SPEED, 1)
             self.wait_until_ready()
             break
 
